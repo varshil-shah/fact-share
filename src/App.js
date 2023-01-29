ddce9c86f2f36a31e14c586c3fb32e6acc08dc86
Varshil-Shah <varshilashah09@gmail.com>
2023-01-29 14:59:12 +0530

    :sparkles: add a feature to filter facts by categories

--- src/App.js
+++ src/App.js
@@ -3,40 +3,6 @@ import supabase from "./supabase";
 
 import "./style.css";
 
-const initialFacts = [
-  {
-    id: 1,
-    text: "React is being developed by Meta (formerly facebook)",
-    source: "https://opensource.fb.com/",
-    category: "technology",
-    votesInteresting: 24,
-    votesMindblowing: 9,
-    votesFalse: 4,
-    createdIn: 2021,
-  },
-  {
-    id: 2,
-    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-    source:
-      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-    category: "society",
-    votesInteresting: 11,
-    votesMindblowing: 2,
-    votesFalse: 0,
-    createdIn: 2019,
-  },
-  {
-    id: 3,
-    text: "Lisbon is the capital of Portugal",
-    source: "https://en.wikipedia.org/wiki/Lisbon",
-    category: "society",
-    votesInteresting: 8,
-    votesMindblowing: 3,
-    votesFalse: 1,
-    createdIn: 2015,
-  },
-];
-
 const CATEGORIES = [
   { name: "technology", color: "#3b82f6" },
   { name: "science", color: "#16a34a" },
@@ -62,27 +28,37 @@ function Header({ onShowForm, showForm }) {
   );
 }
 
-function CategoryFilter() {
+function CategoryFilter({ setCurrentCategory }) {
   return (
     <aside>
       <ul>
         <li className="category">
-          <button className="btn btn-all-categories">All</button>
+          <button
+            className="btn btn-all-categories"
+            onClick={() => setCurrentCategory("all")}
+          >
+            All
+          </button>
         </li>
         {CATEGORIES.map((category) => (
-          <Category category={category} key={category.name} />
+          <Category
+            category={category}
+            key={category.name}
+            setCurrentCategory={setCurrentCategory}
+          />
         ))}
       </ul>
     </aside>
   );
 }
 
-function Category({ category }) {
+function Category({ category, setCurrentCategory }) {
   return (
     <li className="category">
       <button
         className="btn btn-category"
         style={{ backgroundColor: category.color }}
+        onClick={() => setCurrentCategory(category.name)}
       >
         {category.name}
       </button>
@@ -91,6 +67,16 @@ function Category({ category }) {
 }
 
 function FactsList({ facts }) {
+  if (facts.length === 0) {
+    return (
+      <Message
+        message={
+          "No facts found for this category yet! Create the first one 🤘"
+        }
+      />
+    );
+  }
+
   return (
     <section>
       <ul className="facts-list">
@@ -221,13 +207,18 @@ function App() {
   const [showForm, setShowForm] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [facts, setFacts] = useState([]);
+  const [currentCategory, setCurrentCategory] = useState("all");
 
   useEffect(() => {
     async function getFacts() {
       setIsLoading(true);
-      let { data: facts, error } = await supabase
-        .from("facts")
-        .select("*")
+
+      let query = supabase.from("facts").select("*");
+
+      if (currentCategory !== "all")
+        query = query.eq("category", currentCategory);
+
+      let { data: facts, error } = await query
         .order("votestInteresting", { ascending: false })
         .limit(20);
       if (!error) setFacts(facts);
@@ -235,7 +226,7 @@ function App() {
       setIsLoading(false);
     }
     getFacts();
-  }, []);
+  }, [currentCategory]);
 
   const onAddNewFact = (newFact) => setFacts((facts) => [newFact, ...facts]);
   const onShowForm = (value) => setShowForm(value);
@@ -250,7 +241,7 @@ function App() {
         <FactForm onAddNewFact={onAddNewFact} onShowForm={onShowForm} />
       )}
       <main className="main">
-        <CategoryFilter />
+        <CategoryFilter setCurrentCategory={setCurrentCategory} />
         {isLoading ? <Message /> : <FactsList facts={facts} />}
       </main>
     </>
