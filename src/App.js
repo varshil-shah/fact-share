e7c8e7f7913f88503bb4873e3bb3dce28662754c
Varshil-Shah <varshilashah09@gmail.com>
2023-01-29 11:16:24 +0530

    :art: passing and receiving props for facts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,51 @@
 import "./style.css";
 
-function App() {
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+function Header() {
   return (
     <header className="header">
       <div className="logo">
@@ -11,6 +56,121 @@ function App() {
         Share a fact
       </button>
     </header>
+  );
+}
+
+function CategoryFilter() {
+  return (
+    <aside>
+      <ul>
+        <li className="category">
+          <button className="btn btn-all-categories">All</button>
+        </li>
+        <li className="category">
+          <button
+            className="btn btn-category"
+            style={{ backgroundColor: "#3b82f6" }}
+          >
+            Technology
+          </button>
+        </li>
+        <li className="category">
+          <button
+            className="btn btn-category"
+            style={{ backgroundColor: "#16a34a" }}
+          >
+            Science
+          </button>
+        </li>
+        <li className="category">
+          <button
+            className="btn btn-category"
+            style={{ backgroundColor: "#ef4444" }}
+          >
+            Finance
+          </button>
+        </li>
+      </ul>
+    </aside>
+  );
+}
+
+function FactsList() {
+  // Temporary
+  const facts = initialFacts;
+
+  return (
+    <section>
+      <ul className="facts-list">
+        {facts.map((fact) => (
+          <Fact fact={fact} key={fact.id} />
+        ))}
+      </ul>
+      <p>There are {facts.length} facts in the database. Add your own!</p>
+    </section>
+  );
+}
+
+function Fact({ fact }) {
+  return (
+    <li className="fact">
+      <p>
+        {fact.text}
+        <a
+          className="source"
+          href={fact.source}
+          target="_blank"
+          rel="noreferrer"
+        >
+          (Source)
+        </a>
+      </p>
+      <span
+        className="tag"
+        style={{
+          backgroundColor: CATEGORIES.find(
+            (category) => category.name === fact.category
+          ).color,
+        }}
+      >
+        {fact.category}
+      </span>
+      <div className="vote-buttons">
+        <button>👍 {fact.votesInteresting}</button>
+        <button>🤯 {fact.votesMindblowing}</button>
+        <button>⛔️ {fact.votesFalse}</button>
+      </div>
+    </li>
+  );
+}
+
+function FactForm() {
+  return (
+    <form className="fact-form">
+      <input type="text" placeholder="Share a fact with the world..." />
+      <span>200</span>
+      <input type="text" placeholder="Trustworthy source..." />
+      <select>
+        <option value="">Choose category:</option>
+        <option value="technology">Technology</option>
+        <option value="science">Science</option>
+        <option value="finance">Finance</option>
+      </select>
+      <button className="btn btn-large">Post</button>
+    </form>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Header />
+      <FactForm />
+      <main className="main">
+        <CategoryFilter />
+        <FactsList />
+      </main>
+    </>
   );
 }
 
